b07e99d9dc2f2c4c8f1ade869f0d2d02448993d7
Nandini Agrawal <nandini.agrawal@blueyonder.com>
2020-10-14 17:05:22 +0530

    added thread changes

--- src/main/java/com/demo/rege/Controller/RegController.java
+++ src/main/java/com/demo/rege/Controller/RegController.java
@@ -7,24 +7,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 @RestController
 public class RegController {
-
-    @PostMapping("/api/{regex}/{textBody}")
+	
+	@PostMapping("/api/{regex}/{textBody}")
     public Response getResponse(@PathVariable("regex") String regex, @PathVariable("textBody") String textBody) {
         Response response = new Response();
         try {
-            Pattern r = Pattern.compile(regex);
-            Matcher m = r.matcher(textBody);
-            if (m.find()) {
-                response.setMatch(m.group(0));
-            } else {
-                response.setMatch(null);
-            }
-            response.setError(false);
-        } catch (PatternSyntaxException exception) {
+            Pattern r = Pattern.compile(regex); 
+            Runnable runnable = new Runnable() {
+   	         @Override
+   	         public void run()
+   	         {
+   	            Matcher interruptableMatcher = r.matcher(new InterruptibleCharSequence(textBody));
+   	            if(interruptableMatcher.find()) {
+   	            	response.setMatch(interruptableMatcher.group(0)); 
+   	            }  
+				else {
+   	            	response.setMatch(null);
+   	            }
+   	         }
+   	      };
+
+   	      Thread thread = new Thread(runnable);
+   	      thread.start();
+
+   	      Thread.sleep(500);
+   	      thread.interrupt();
+   	      response.setError(false);
+        } catch (InterruptedException exception ) {
             response.setMatch(null);
             response.setError(true);
         }
